825093b4862d97eb1caf8433383a0fb872808851
Octaviera8 <octaviera_nanda_26rpl@student.smktelkom-mlg.sch.id>
2019-02-12 13:08:12 +0700

    first commit

--- application/views/admin/Supplier/list.php
+++ application/views/admin/Supplier/list.php
@@ -1,9 +1,53 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+table{
+	border-collapse: collapse;
+  width: 100%;
+}
 
-foreach ($supplier as $supplier):
-        echo $supplier->supplier_id;
-        echo $supplier->supplier_name;
-        echo $supplier->supplier_address;
-        echo "<br />";
-endforeach
-?>
+th, td {
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even){background-color: 	#F0F8FF}
+
+th {
+  background-color: #7FFFD4;
+  color: white;
+}
+</style>
+</head>
+<body>
+<table class="table table-hover" id="dataTable" width="100%" cellspacing="0">
+        <thead>
+		<tr>
+		<th>Supplier Id</th>
+		<th>Supplier Nama</th>
+		<th>Supplier Address</th>
+                
+		</tr>
+	</thead>
+	<tbody>
+<?php foreach ($supplier as $supplier): ?>
+	<tr>
+        <td width="150">
+	        <?php echo $supplier->supplier_id ?>
+	</td>
+	<td>
+	        <?php echo $supplier->supplier_name ?>
+	</td>
+        <td>
+		<?php echo $supplier->supplier_address ?>
+        </td>
+        
+                                        
+	</tr>
+                <?php endforeach; ?>
+
+	</tbody>
+        </table>
+</body>
+</html>
